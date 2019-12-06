f4835dd30e965c9745d4596a9f7785029ad01fa9
JR Rickerson <jrrickerson@redrivetstudios.com>
2019-12-06 17:37:06 -0500

    Python: Add day 1 solutions

---
+++ python/day001.py
@@ -0,0 +1,25 @@
+INPUT_FILE = 'day001.txt'
+
+
+def calculate_fuel(mass):
+    fuel = mass // 3 - 2
+    total = 0
+    while fuel > 0:
+        total += fuel
+        fuel = fuel // 3 - 2
+    return total
+
+
+def part1(filename):
+    with open(filename) as f:
+        return sum([int(m.strip()) // 3 - 2 for m in f])
+
+
+def part2(filename):
+    with open(filename) as f:
+        return sum([calculate_fuel(int(m.strip())) for m in f])
+
+
+if __name__ == '__main__':
+    print('Part 1:', part1(INPUT_FILE))
+    print('Part 2:', part2(INPUT_FILE))
